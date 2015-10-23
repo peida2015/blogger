3f50baeab86b5438c01ea0261e7c71783dac6ca1
Pei Da Kuo <pdguo@berkeley.edu>
2015-10-23 23:18:18 +0000

    Sorcery authentication complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,13 @@
 class CommentsController < ApplicationController
+  before_filter :require_login, except: [:create]
+  
+  def require_login
+    unless current_user
+      redirect_to login_path
+      return false
+    end
+  end
+  
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,13 @@
 class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
+  
+  def require_login
+    unless current_user
+      redirect_to login_path
+      return false
+    end
+  end
+  
   def show
     @tag = Tag.find(params[:id])
   end
